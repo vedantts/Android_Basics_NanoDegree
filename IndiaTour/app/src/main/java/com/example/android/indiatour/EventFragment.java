877cd0f039ee10a0996a69c020ac255025b0fc15
vedantts <vedantlj1996@gmail.com>
2017-03-03 23:28:59 +0530

    Seventh Project

---
+++ IndiaTour/app/src/main/java/com/example/android/indiatour/EventFragment.java
@@ -0,0 +1,49 @@
+package com.example.android.indiatour;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link EventFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link EventFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EventFragment extends Fragment {
+    public EventFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+        final ArrayList<Detail> detail = new ArrayList<>();
+        detail.add(new Detail(R.string.event_one, R.string.event_review_one));
+        detail.add(new Detail(R.string.event_two, R.string.event_review_two));
+        detail.add(new Detail(R.string.event_three, R.string.event_review_three));
+        detail.add(new Detail(R.string.event_four, R.string.event_review_four));
+        detail.add(new Detail(R.string.event_five, R.string.event_review_five));
+        detail.add(new Detail(R.string.event_six, R.string.event_review_six));
+        detail.add(new Detail(R.string.event_seven, R.string.event_review_seven));
+        detail.add(new Detail(R.string.event_eight, R.string.event_review_eight));
+
+        Adapter adapter = new Adapter(getActivity(), detail, R.color.category_events);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+        return rootView;
+    }
+}
---
+++ IndiaTour/app/src/main/java/com/example/android/indiatour/ReviewFragment.java
@@ -0,0 +1,43 @@
+package com.example.android.indiatour;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+
+
+public class ReviewFragment extends Fragment {
+    public ReviewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+        final ArrayList<Detail> detail = new ArrayList<>();
+        detail.add(new Detail(R.string.event_one, R.string.event_review_one));
+        detail.add(new Detail(R.string.event_two, R.string.event_review_two));
+        detail.add(new Detail(R.string.event_three, R.string.event_review_three));
+        detail.add(new Detail(R.string.event_four, R.string.event_review_four));
+        detail.add(new Detail(R.string.event_five, R.string.event_review_five));
+        detail.add(new Detail(R.string.event_six, R.string.event_review_six));
+        detail.add(new Detail(R.string.event_seven, R.string.event_review_seven));
+        detail.add(new Detail(R.string.event_eight, R.string.event_review_eight));
+
+        Adapter adapter = new Adapter(getActivity(), detail, R.color.category_reviews);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+        return rootView;
+    }
+}
